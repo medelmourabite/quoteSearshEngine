e94a4364fa08417e8362806d9e60d0c07ed9a2fd
MedEM <med.elmourabite@gmail.com>
2018-12-30 21:37:35 +0100

    fixed unit tests

--- test/math-spec.js
+++ test/math-spec.js
@@ -1,19 +1,19 @@
 var expect = require("chai").expect;
-var assert = require('chai').assert;
+var assert = require("chai").assert;
 var exemple = require("../src/math-module");
 
-describe("" , function () {
-    describe("addition()", function () {
-        it("Retourne la somme de 2 nombres", function () {
-           var resultat = exemple.addition(6,2);
-           expect(resultat).to.equal(8);
-        });  
+describe("Module Math", function() {
+  describe("addition()", function() {
+    it("Retourne la somme de 2 nombres", function() {
+      var resultat = exemple.addition(6, 2);
+      expect(resultat).to.equal(8);
     });
-    
-    describe("typeDe()", function () {
-        it('Le type du retour doit etre un entier', function () {
-           var resultat = exemple.addition(4,5);
-            assert.typeOf(resultat, 'number');
-        });     
-    }); 
+  });
+
+  describe("typeDe()", function() {
+    it("Le type du retour doit etre un entier", function() {
+      var resultat = exemple.addition(4, 5);
+      assert.typeOf(resultat, "number");
+    });
+  });
 });
--- test/mocha-spec.js
+++ test/mocha-spec.js
@@ -2,16 +2,33 @@ var rewire = require("rewire");
 var chai = require("chai");
 var expect = chai.expect;
 chai.config.includeStack = false;
+var exemple = require("../src/math-module");
+
 describe("Mon test unitaire", function() {
   beforeEach(function() {
-	  //console.log("Test debut");
+    //console.log("Test debut");
   });
   afterEach(function() {
-	  	  //console.log("Test fin");
-
+    //console.log("Test fin");
   });
   it("Un test qui passe toujours", function(done) {
-    expect(true).to.be.equal(false);
+    expect(true).to.be.equal(true);
     done();
+  });
+
+  describe("Module Math", function() {
+    describe("addition()", function() {
+      it("Retourne la somme de 2 nombres", function() {
+        var resultat = exemple.addition(6, 2);
+        expect(resultat).to.equal(8);
+      });
+    });
+
+    // describe("typeDe()", function() {
+    //   it("Le type du retour doit etre un entier", function() {
+    //     var resultat = exemple.addition(4, 5);
+    //     assert.typeOf(resultat, typeof resultat);
+    //   });
+    // });
   });
 });
