b8a5ca3b04f48668b8d9419baffebe8ce0681ba5
MedEM <med.elmourabite@gmail.com>
2018-12-30 21:13:48 +0100

    added unit testing

---
+++ src/math-module.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //Addition de 2 nombre
+    //a,b : nombre réels
+    //return : somme de a et b
+    addition(a,b){
+        return a + b;
+    }
+}
--- test/mocha-spec.js
+++ test/mocha-spec.js
@@ -2,10 +2,15 @@ var rewire = require("rewire");
 var chai = require("chai");
 var expect = chai.expect;
 chai.config.includeStack = false;
-describe("My test suite", function() {
-  beforeEach(function() {});
-  afterEach(function() {});
-  it("Test case", function(done) {
+describe("Mon test unitaire", function() {
+  beforeEach(function() {
+	  //console.log("Test debut");
+  });
+  afterEach(function() {
+	  	  //console.log("Test fin");
+
+  });
+  it("Un test qui passe toujours", function(done) {
     expect(true).to.be.equal(true);
     done();
   });
